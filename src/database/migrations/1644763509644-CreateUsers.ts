561446e92e9fc624603a909fab09e9ae4d184a40
Thayana Classo Soares <t.classo.soares@avanade.com>
2022-02-13 12:16:47 -0300

    entity-created

---
+++ src/database/migrations/1644763509644-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1644763509644 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,12 @@
-import express, { response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
 
 //atua nas rotas 
 const app = express();
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica (alterar o avatar do usuário)
- */
-
-
 //rotas
-app.get("/teste", (request, response)=> {
-  //Request => Entrando
-  //Response => Saindo
-  return response.send("Olá mundo")
-})
-
-app.post("/teste-post", (request, response) => {
-  return response.send("Olá mundo post")
-})
-
-
-
 // iniciando o servidor
 app.listen(3000, () => console.log("Server is running")); 
 
