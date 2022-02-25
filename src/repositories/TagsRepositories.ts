09fb774b6a994263fa3023cbcf1ba21cdb0406a6
Thayana Classo Soares <t.classo.soares@avanade.com>
2022-02-25 18:16:10 -0300

    table-relationship

--- src/repositories/TagsRepositories.ts
+++ src/repositories/TagsRepositories.ts
@@ -2,7 +2,6 @@ import { EntityRepository, Repository } from "typeorm";
 import { Tag } from "../entities/Tag";
 
 @EntityRepository(Tag)
-
 class TagsRepositories extends Repository<Tag>{
 }
 export { TagsRepositories };
